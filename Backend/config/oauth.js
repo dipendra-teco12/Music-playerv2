df4b258ced6e68fe4a1e82cc3a0264d0823aaa64
dipendra-teco12 <dipendra.patel@tecocraft.com>
2025-07-25 18:07:25 +0530

    minor change css link added in login

--- Backend/config/oauth.js
+++ Backend/config/oauth.js
@@ -13,22 +13,24 @@ passport.use(
       try {
         // 1. Check if the user already exists
         const email = profile.emails[0].value;
-        let user = await User.findOne({ googleId: profile.id });
+        // let user = await User.findOne({ googleId: profile.id });
 
+        let user = await User.findOne({ email });
         if (user) {
           return done(null, user);
         }
 
         // 2. If not, create a new user
+
         user = new User({
           googleId: profile.id,
           fullName: profile.displayName,
           email: profile.emails[0].value,
-          role: "user", 
+          role: "user",
         });
 
         await user.save();
-        return done(null, user); 
+        return done(null, user);
       } catch (err) {
         return done(err, null);
       }
